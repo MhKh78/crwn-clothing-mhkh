10523f56eb2ce28f67dde33435c19211e79bb82e
MhKh78 <mhkh431@gmail.com>
2019-08-16 13:57:53 +0430

    HomePage and MenuItem And Style Done

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Directory from './../../components/directory/directory.componet';
+
+import './homepage.styles.scss';
+
+const HomePage = () => {
+  return (
+    <div className='homepage'>
+      <Directory />
+    </div>
+  );
+};
+
+export default HomePage;
