ff8cc29cfc13c9d6133108074d4c811081e6fccb
MhKh78 <mhkh431@gmail.com>
2019-08-18 15:33:35 +0430

    added shop page and styles for it

--- src/App.js
+++ src/App.js
@@ -4,19 +4,13 @@ import { Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
-
-const HatsPage = props => (
-  <div>
-    <button onClick={() => props.history.push('/topics')}>Topics</button>
-    <h1>HATS PAGE</h1>
-  </div>
-);
+import ShopPage from './pages//shop/shop.compoent';
 
 function App() {
   return (
     <div>
       <Route exact path='/' component={HomePage} />
-      <Route path='/shop/hats' component={HatsPage} />
+      <Route path='/shop' component={ShopPage} />
     </div>
   );
 }
