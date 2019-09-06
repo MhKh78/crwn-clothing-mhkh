bfc1f8379155d2d8ba4134dfc8954c40fa8d6449
MhKh78 <mhkh431@gmail.com>
2019-09-06 21:57:49 +0430

    Added remove to checkout

--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,4 +1,6 @@
 export const CartActionTypes = {
   TOGGLE_CART_HIDDEN: 'toggle_cart_hidden',
-  ADD_ITEM: 'add_item'
+  ADD_ITEM: 'add_item',
+  REMOVE_ITEM: 'remove_item',
+  CLEAR_ITEM_FROM_CART: 'clear_item_from_cart'
 };
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -13,3 +13,22 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
 
   return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
 };
+
+export const clearItemFromCart = (cartItems, cartItemToClear) =>
+  cartItems.filter(cartItem => cartItem.id !== cartItemToClear.id);
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  const existingCartItem = cartItems.find(
+    cartItem => cartItem.id === cartItemToRemove.id
+  );
+
+  if (existingCartItem.quantity === 1) {
+    return clearItemFromCart(cartItems, cartItemToRemove);
+  }
+
+  return cartItems.map(cartItem =>
+    cartItem.id === cartItemToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem
+  );
+};
